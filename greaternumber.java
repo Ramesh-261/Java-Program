51f1a0535e43c0f7c13da8aacb9f70f9570e5387
Ramesh2611 <rp572493rameshkumar@gmail.com>
2024-06-30 22:51:37 +0530

    ssa

--- greaternumber.java
+++ greaternumber.java
@@ -5,7 +5,7 @@ public class greaternumber {
         int num1=sc.nextInt();
         int num2=sc.nextInt();
         if(num1>num2){
-            System.out.println("number is greaters :" +num1);
+            System.out.println("numdsdsber is greaters :" +num1);
         }else{
             System.out.println("number is greaters : "+num2);
         }
