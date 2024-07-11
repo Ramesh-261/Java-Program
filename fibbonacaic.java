0754e97f71e959bdf48d4ae60919e15bc8530e74
Ramesh2611 <rp572493rameshkumar@gmail.com>
2024-07-11 19:29:55 +0530

    Fibbonaci

--- fibbonacaic.java
+++ fibbonacaic.java
@@ -9,7 +9,7 @@ public class fibbonacaic
 	    System.out.print(n1+" "+n2);
 	    for(int i=2;i<=cnt;i++){
 	        n3=n1+n2;
-	        System.out.print(" "+n3);
+	        System.out.print(" "+n3);//for n3 used
 	        n1=n2;
 	        n2=n3;
 	    }
