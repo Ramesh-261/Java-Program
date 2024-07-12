3ed1b720f3625db51df37198dd4a549b04d39936
Ramesh2611 <rp572493rameshkumar@gmail.com>
2024-07-12 19:56:02 +0530

    count of digits and digits

---
+++ cntprint.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class cntprint {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String str = Integer.toString(n);
+        int len = str.length();
+        System.out.println(len);
+        for (int i = len - 1; i >= 0; i--) {
+            System.out.println(str.charAt(i));
+        }
+        sc.close();
+    }
+}
