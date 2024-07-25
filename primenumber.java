e536afbd0d90bb8f2ad0ad66c58b50f3465187b5
Ramesh2611 <rp572493rameshkumar@gmail.com>
2024-07-25 20:15:33 +0530

    PrimeNumber

--- primenumber.java
+++ primenumber.java
@@ -2,9 +2,7 @@ import java.util.Scanner;
 public class primenumber
 {
 	public static void main(String[] args) {
-
         Scanner sc=new Scanner(System.in);
-
 	    int n=sc.nextInt();
 	    int tmp=0;
 	    for(int i=2;i<=Math.sqrt(n);i++){
