fe40e13ea866b0d673ec9c7c4985cb98d4e5844a
Ramesh2611 <rp572493rameshkumar@gmail.com>
2024-07-25 20:23:29 +0530

    ArrMax

--- arrmax.java
+++ arrmax.java
@@ -10,6 +10,7 @@ public class arrmax {
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
+        //Logic part below
         int max=arr[0];
         for(int i=1;i<n;i++){
             if(arr[i]>max){
