9b67e9b6901ff96aaaf6ccca9ea7e2385094480d
Ramesh Prajapati <134092313+Ramesh-261@users.noreply.github.com>
2024-08-21 20:11:29 +0530

    Update addsumarr.java

--- addsumarr.java
+++ addsumarr.java
@@ -1,10 +1,7 @@
 //import java.io.*;
 import java.util.*;
-
 public class addsumarr {
-
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int[] arr=new int[n];
